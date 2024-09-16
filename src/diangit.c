85d557f9a08d314d518c97229dfcd0c621aa811f
James Wu <1296702023@qq.com>
2024-09-17 00:24:11 +0800

    增强程序适应性

--- src/diangit.c
+++ src/diangit.c
@@ -6,23 +6,25 @@
 int main(int argc,char *argv[]){
     const char *dir=NULL;
 
-    //是否提供了目录
-    if((argc!=3&&argc!=2)||strcmp(argv[1], "init") != 0){
-        fprintf(stderr,"Wrong input!\nTry using: diagit init <directory>\n");
-        return 1;
-    }else if(argc==2&&strcmp(argv[1], "init")==0){
-        char path_dir[1024];
-        GetCurrentDirectory(sizeof(path_dir),path_dir);
-        dir=path_dir;
-    }else{
-        dir=argv[2];
-    }
+    //进行初始化
+    if(strcmp(argv[1], "init")==0){
+        //是否提供了目录
+        if((argc!=3&&argc!=2)){
+            fprintf(stderr,"Wrong input!\nTry using: diagit init <directory>\n");
+            return 1;
+        }else if(argc==2){
+            char path_dir[1024];
+            GetCurrentDirectory(sizeof(path_dir),path_dir);
+            dir=path_dir;
+        }else{
+            dir=argv[2];
+        }
 
-    if(DiangitInit(dir)){
-        printf("Initialize git  repository successfully.\n");
-    }else{
-        printf("Fail to initialize git repository.\n");
+        if(DiangitInit(dir)){
+            printf("Initialize git  repository successfully.\n");
+        }else{
+            printf("Fail to initialize git repository.\n");
+        }
     }
-
     return 0;
 }
