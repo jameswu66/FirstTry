4b50ed563e74889ae2ce13e3307c569a1c4cf1c9
James Wu <1296702023@qq.com>
2024-09-19 20:06:47 +0800

    step2:内存不够？原来可以实现的功能现在不行了

--- src/diangit.c
+++ src/diangit.c
@@ -2,15 +2,22 @@
 #include "Init.h"
 #include <string.h>
 #include <windows.h>
+#include "hash.h"
+#include <openssl/evp.h>
 
 int main(int argc,char *argv[]){
     const char *dir=NULL;
+
+    if(argc<2){
+        fprintf(stderr,"Wrong input!\nTry using: diangit <command>, ...");
+        return 1;
+    }
 
     //进行初始化
     if(strcmp(argv[1], "init")==0){
         //是否提供了目录
         if((argc!=3&&argc!=2)){
-            fprintf(stderr,"Wrong input!\nTry using: diagit init <directory>\n");
+            fprintf(stderr,"Wrong input!\nTry using: diangit init <directory>\n");
             return 1;
         }else if(argc==2){
             char path_dir[1024];
@@ -25,6 +32,38 @@ int main(int argc,char *argv[]){
         }else{
             printf("Fail to initialize git repository.\n");
         }
+    }else if(strcmp(argv[1],"hash-object")==0){
+        if(argc!=3){
+            fprintf(stderr,"Wrong input!\nTry using: diangit hash-object <filename>\n");
+            return 1;
+        }
+
+        unsigned char hash[EVP_MAX_MD_SIZE];
+        SHA1(argv[2],hash);
+
+        //需要将得到的二进制转换成十六进制字符串作为可阅读的哈希值
+        char objecthash[EVP_MAX_MD_SIZE*2+1];//每个字节转换成一个十六进制
+        for(int i=0;i<EVP_MD_size(EVP_sha1());i++){
+            //i小于哈希算法生成的哈希值的长度（SHA-1）
+            sprintf(&objecthash[i*2],"%02x",hash[i]);//每两个hash[]中的内容存储在objecthash[]
+        }
+
+        if(StoreSHA(argv[2],objecthash)){
+            printf("We store your object in hash: %s\n",objecthash);//表明存入了以哈希值命名的文件
+        }else{
+            printf("Fail when storing your object!\n");
+        }
+    }else if(strcmp(argv[1],"cat-file")==0){
+        if(argc!=3){
+            fprintf(stderr,"Wrong input!\nTry using: diangit cat-file <hash of object>\n");
+            return 1;
+        }
+
+        if(!Catfile(argv[2])){
+            printf("Fail when reading your object!\n");
+        }
+    }else{
+        fprintf(stderr,"Unknown command\n");
     }
     return 0;
 }
